8688694a6b8c2893233e83a9bf0a4524d2ad3186
Edu Blanco <e.blancof@edu.uah.es>
2023-12-20 11:11:49 +0000

    Some changes in code

--- Lora_scripts/backend/run_mod.py
+++ Lora_scripts/backend/run_mod.py
@@ -31,10 +31,10 @@ collected_samples = int(os.getenv("COLLECTED_SAMPLES"))
 collected_time_iteration = t_sample*collected_samples
 buffer_time_ms = int(os.getenv("BUFFER_TIME_MS"))
 buffer_len = int((buffer_time_ms/1000)//collected_time_iteration)
-pwr_buffer = deque([(0,0)] * buffer_len, maxlen=buffer_len)
+energy_buffer = deque([(0,0)] * buffer_len, maxlen=buffer_len)
 buffer_index = 0
 extra_threshold = float(os.getenv("EXTRA_THRESHOLD"))
-pwr_threshold = 0
+energy_thresh_buffer = 0
 auto_thresh = bool(os.getenv("AUTOTHRESH"))
 pxx = []
 power_result = 0
@@ -50,7 +50,6 @@ client_id = str(os.getenv("CLIENT_ID"))
 client = connect_mqtt(client_id=client_id, broker=broker, port=port)
 if client:
     client.loop_start()
-client = True
 ### UDP Config ###
 udp_host = str(os.getenv("UDP_HOST"))
 udp_port = int(os.getenv("UDP_PORT"))
@@ -99,33 +98,34 @@ async def initial_measurement():
     initial_collected_samples = collected_samples *10
     index = 0
     
-    power_threshold = 0
+    energy_threshold = 0
     async for samples in sdr_initial.stream(num_samples_or_bytes=initial_collected_samples):
         # Calculate the power threshold based on the initial measurement
-        _, initial_pxx = welch(x=samples, fs=sdr_initial.center_freq, nperseg=nfft, scaling='spectrum',
-                               return_onesided=False)
-        pxx = deque(initial_pxx)
-        pxx.rotate(rotation)
-        initial_power = np.trapz(pxx, f)  # Integrate in linear scale
-        power_threshold += initial_power
-        print(f"Initial power: {initial_power:.10f}")
+        #_, initial_pxx = welch(x=samples, fs=sdr_initial.center_freq, nperseg=nfft, scaling='spectrum',
+        #                       return_onesided=False)
+        #pxx = deque(initial_pxx)
+        #pxx.rotate(rotation)
+        #initial_power = np.trapz(pxx, f)  # Integrate in linear scale
+        init_energy_norm = np.sum(np.square(np.abs(samples)))/len(samples)
+        energy_threshold += init_energy_norm
+        print(f"Initial energy per sample: {init_energy_norm:.10f}")
         index += 1
         if index > buffer_len:
             break
     sdr_initial.cancel_read_async()
     sdr_initial.close()
-    print("Initial power threshold: ", power_threshold)
-    return power_threshold
+    print("Initial power threshold: ", energy_threshold)
+    return energy_threshold
 
 
 async def processing_task(udp_thread_instance,mqtt_thread_instance):
-    global sdr, pwr_threshold, buffer_index
+    global sdr, energy_thresh_buffer, buffer_index
     if auto_thresh:
-        pwr_threshold = await initial_measurement()
+        energy_thresh_buffer = await initial_measurement()
     else:
-        pwr_threshold = float(os.getenv("POWER_THRESHOLD"))
+        energy_thresh_buffer = float(os.getenv("ENERGY_THRESHOLD"))
     if auto_thresh:
-        pwr_threshold *= (1 + extra_threshold)
+        energy_thresh_buffer *= (1 + extra_threshold)
     # Empezamos ambos hilos de forma paralela
     if client:
         mqtt_thread_instance.start()
@@ -136,8 +136,8 @@ async def processing_task(udp_thread_instance,mqtt_thread_instance):
     signal.signal(signal.SIGINT, handle_sigint)
     try:
         async for samples in sdr.stream(num_samples_or_bytes=collected_samples):
-            pwr, pxx = await process_samples(samples, sdr.center_freq)
-            pwr_buffer.appendleft([pwr, buffer_index])
+            energy_norm = process_samples(samples)     
+            energy_buffer.appendleft([energy_norm, buffer_index])
             buffer_index += 1 
             if buffer_index > buffer_len:
                 buffer_index = 0
@@ -145,27 +145,34 @@ async def processing_task(udp_thread_instance,mqtt_thread_instance):
         sys.exit(0)
 
 
-async def process_samples(samples, sdr_freq):
+def process_samples(samples):
     global udp_buffer
-    start_time = time.time()
-    _, pxx = welch(x=samples, fs=sdr_freq, nperseg=nfft, scaling='spectrum', return_onesided=False)
-    pxx = deque(pxx)
-    pxx.rotate(rotation)
-    power_result = np.trapz(pxx, f)  # Integrate in linear scale
-    pxx = 10 * np.log10(pxx)
-    end_time = time.time()
-    elapsed_time = end_time - start_time
+    #start_time = time.time()
+    #_, pxx = welch(x=samples, fs=sdr_freq, nperseg=nfft, scaling='spectrum', return_onesided=False)
+    #pxx = deque(pxx)
+    #pxx.rotate(rotation)
+    #power_result = np.trapz(pxx, f)  # Integrate in linear scale
+    #pxx = 10 * np.log10(pxx)
+    #end_time = time.time()
+    #elapsed_time = end_time - start_time
     #print(f"Time taken for data collection and processing: {elapsed_time:.5f} seconds, power={power_result:.10f}")
-    process_udp(pxx, power_result)
-    return power_result, pxx
+    energy_norm = np.sum(np.square(np.abs(samples)))/len(samples)
+    print(energy_norm)
+    process_udp(samples, energy_norm)
+    return energy_norm
 
 
-def process_udp(pxx, power_result):
+def process_udp(samples, energy_norm):
     data = {
         "frequencies": f,
-        "pxx": np.float32(pxx),
-        "power_result": power_result,
-        "isOverThreshold": False#power_result > pwr_threshold,
+        "samples": samples,
+        "energy_norm": energy_norm,
+        "parameters": {
+            "nfft": nfft,
+            "sdr_freq": sdr.center_freq,
+            "energy_thresh_buffer": energy_thresh_buffer,
+            "extra_threshold": extra_threshold,
+        }
     }
     data_bytes = pickle.dumps(data)
     udp_buffer.append(data_bytes)
@@ -174,10 +181,10 @@ def process_udp(pxx, power_result):
 def mqtt_thread():
     global buffer_index
     while True:
-        buffer = pwr_buffer.copy()
+        buffer = energy_buffer.copy()
         if buffer[0][1] == buffer.maxlen:
-            sum_pwr = sum([x[0] for x in buffer])
-            msg = f"Power: {sum_pwr:.6f} Detected intrusion" if sum_pwr > pwr_threshold else "No intrusion detected"
+            sum_energy_buffer = sum([x[0] for x in buffer])
+            msg = f"Energy: {sum_energy_buffer:.6f} Detected intrusion" if sum_energy_buffer > energy_thresh_buffer else "No intrusion detected"
             publish_mqtt(msg)
             print(msg)
             
@@ -185,17 +192,18 @@ def mqtt_thread():
 
 
 def publish_mqtt(msg):
-    time.sleep(0.2)
-    try:
-        result = client.publish(topic, msg)
-    except Exception as e:
-        print(e)
-    # result: [0, 1]
-    status = result[0]
-    if status == 0:
-        print(f"Message `{msg}` sent to topic `{topic}`")
-    else:
-        print(f"Failed to send message to topic {topic}")
+    time.sleep(.1)
+    if client:
+        try:
+            result = client.publish(topic, msg)
+        except Exception as e:
+            print(e)
+        # result: [0, 1]
+        status = result[0]
+        if status == 0:
+            print(f"Message `{msg}` sent to topic `{topic}`")
+        else:
+            print(f"Failed to send message to topic {topic}")
 
 
 def udp_thread():
